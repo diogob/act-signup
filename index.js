0eff81ab2739493b0c0ba754dc8940aa0cbc0682
Diogo Biazus <diogob@gmail.com>
2016-06-09 23:41:13 -0400

    Add user inputs to model and reducer

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 import main from '@act/main'
 import value from '@act/main/processes/value'
+import { get, post } from '@act/main/signals/sources/xhr'
+import { when, equals } from 'ramda/src/compose'
 
-const view = (name) => (
+const login = (_, history) => {
+  history.push({ type: 'loading' })
+
+  post('http://localhost:3000/rpc/login', model.inputs)(
+    ({ items }) => history.push({ type: 'success', payload: items }),
+    (msg) => history.push({ type: 'loginFailed', payload: msg })
+  )
+}
+
+const view = (model) => (
   ['main', [
-    ['h1', ['Hello ', name]],
-    ['input', {keyup: {update: value}, defaultValue: name}]
+    ['h1', ['User Signup ']],
+    ['label', {for: 'email'}, ['Email']],
+    ['input#email', {change: {email: value}}],
+    ['label', {for: 'password'}, ['Password']],
+    ['input#password', {change: {pass: value}}],
+    ['button', {click: login}, ['Login']],
+    model.loading
+      ? ['div', 'Loading...']
+      : [],
+    model.error && ['div', {style: 'background: red padding: 10px'}, model.error]
   ]]
-
 )
-main(view, { model: 'Joe' })
+
+const model = {
+  error: '',
+  loading: false,
+  inputs: {
+    email: '',
+    pass: ''
+  }
+}
+
+const reducer = (state, {type, payload}) => {
+  switch (type) {
+    case 'success':
+      return { ...state, error: '', loading: false }
+    case 'loading':
+      return { ...state, error: '', loading: true }
+    case 'loginFailed':
+      return { ...state, error: payload, loading: false }
+    case 'email':
+      return { ...state, inputs: { ...state.inputs, email: payload } }
+    case 'pass':
+      return { ...state, inputs: { ...state.inputs, pass: payload } }
+    default:
+      console.log(type, ': ', payload, ' -> ', state)
+      return state
+  }
+}
+
+main(view, {model, reducer})
